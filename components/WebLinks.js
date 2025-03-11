c5a0a6313638cf4f33e897e0d83fabf2f41ec468
Agus Fahmi Aji Pramana <sensai.fahmi1@gmail.com>
2025-03-11 13:48:45 +0700

    update

--- components/WebLinks.js
+++ components/WebLinks.js
@@ -49,9 +49,9 @@ const Links = () => {
     return el.type === "social" && el.on
   });
 
-  // Get data for install section
-  const install = allLinks.filter((el) => {
-    return el.type === "install" && el.on
+  // Get data for featured section
+  const featured = allLinks.filter((el) => {
+    return el.type === "featured" && el.on
   });
 
   // Get data for nfts
@@ -122,13 +122,13 @@ const Links = () => {
               </LinkSection>
               {/* Social Icon */}
 
-              {/* Install Section */}
+              {/* featured Section */}
               {
-                install.length > 0 ?
+                featured.length > 0 ?
                     <LinkSection>
-                      <h3>{install[0].type}</h3>
+                      <h3>{featured[0].type}</h3>
                       {
-                        install.map((i) => {
+                        featured.map((i) => {
                           return (
                               <a href={i.url} key={i.title} target="_blank" rel="noreferrer">
                                 <LinkBox>
@@ -140,7 +140,7 @@ const Links = () => {
                       }
                     </LinkSection> : ''
               }
-              {/* End Install Section */}
+              {/* End featured Section */}
 
               {/* NFT Section */}
               {
