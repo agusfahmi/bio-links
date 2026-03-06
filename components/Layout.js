8198e5c6f39c7cf4b95deb05c624855d645c4d75
agusfahmi <sensai.fahmi1@gmail.com>
2026-03-06 22:03:27 +0700

    revision

--- components/Layout.js
+++ components/Layout.js
@@ -1,31 +1,5 @@
 import styled from 'styled-components'
 import ReactDOM from 'react-dom';
-import Script from "next/script";
-
-export default function RootLayout({ children }) {
-  return (
-    <html>
-      <body>
-        {children}
-
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-LY8HM9EH75"
-          strategy="afterInteractive"
-        />
-
-        <Script id="ga-script" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-LY8HM9EH75');
-          `}
-        </Script>
-      </body>
-    </html>
-  );
-}
-
 
 export default function Layout({ children }) {
     return (
